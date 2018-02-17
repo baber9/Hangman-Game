546b26dcbd94104cb70609577538694c3cecd0e4
Bryan Aber <baber9@gmail.com>
2018-02-17 01:17:19 -0600

    made js dryer, worked on but did not complete bootstrap/css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,18 @@
 var hangman = function () {
     // Variables
+        // To hold letters guessed
     var lettersGuessed = [];
+        // Set to html id to track guesses remaining
     var guessesRemaining = document.getElementById("guesses-remaining");
+        // Set to html id to track wins
     var wins = document.getElementById("wins");
+        // Set to html id to manipulate for blanks/letters
     var answer = document.getElementById("answer");
-    
+        // Set to html id to manipulate player instructions
+    var instruction = document.getElementById("instruction");
+        // initialize userAnswerArray - will use this to build user's answer as they play
+    var userAnswerArray = [];
+        // Answer bank with identifier to build team element later
     var answerBank = [
         {mascot: "Arizona Cardinals", identifier: "ARI"},   
         {mascot: "Atlanta Falcons", identifier: "ATL"},
@@ -40,16 +48,28 @@ var hangman = function () {
         {mascot: "Washington Redskins", identifier: "WAS"}
     ];
 
-    //Randomly select answer, make uppercase, make it an array
+        // Randomly select answer, make uppercase, make it an array
     var randomNum = Math.floor(Math.random() * 32);
     var currentAnswer = answerBank[randomNum].mascot.toUpperCase().split("");
+        
+    getBlanks(currentAnswer, userAnswerArray);
     console.log(currentAnswer);
 
-    var blanks = displayBlanks(currentAnswer);
+    // FUNCTION: To build blanks for puzzle (pushes blanks in place of answer letters)
+    function getBlanks (currAnsArr, userAnsArr) {
+        currAnsArr.forEach(element => {
+            if (element === " ")
+                userAnsArr.push(" ");
+            else    
+                userAnsArr.push("__");
+        });
+        displayWord(userAnsArr);
 
+    }
 
     // INIT:  onKeyUp Function
     document.onkeyup = function(event) {
+
         // change input to uppercase
         var inputLetter = event.key.toUpperCase();   
         // find keycode
@@ -57,69 +77,48 @@ var hangman = function () {
         
         // send keycode & letter to input Handler               
         inputHandler(inputWhich, inputLetter);
-        console.log(randomNum);
-  
     }
 
-    // Initial Display of Blanks as Answer
-    function displayBlanks(word) {
-        console.log(word);
-        var blanks = '';
-        var blanksArray = [];
-        word.forEach(element => {
-            if (element === " ") {
-                blanks +=   "\r\n";     //"\xa0\xa0";
-                blanksArray.push(" ");
-            }
-            else {
-                blanks += "__ ";
-                blanksArray.push("__");
-            }
-        });
-        answer.textContent = blanks;
-        return blanksArray;
-    }
-
-    // INPUT HANDLER FUNCTION
+    // FUNCTION: INPUT HANDLER
     function inputHandler(keyWhich, letter) {
     
-        // Make sure it's a letter
+            // Make sure it's a letter
         if (keyWhich >= 65 && keyWhich <= 90) { 
         
-            // If letter is not in puzzle
+                // If letter is not in puzzle
             if (currentAnswer.indexOf(letter) === -1 && letter != " ") {
-                // Check if letter has alread been guessed
+                    // Check if letter has alread been guessed
                 if (lettersGuessed.indexOf(letter) === -1) {
-                    // add to lettersGuessed array
+                        // Add to lettersGuessed array
                     lettersGuessed.push(letter);
-                    // display letters guessed
+                        // Display letters guessed
                     displayLettersGuessed();
-                    // decrement guesses remaining
+                        // Decrement guesses remaining
                     guessesRemaining.textContent--;
+                        // Check if any guesses remaining, if not game over-lose
                     if (guessesRemaining.textContent === "0")
                         gameOver("lose");
                 }
             }
-            // letter is in puzzle
+                // Letter is in puzzle
             else if (currentAnswer.indexOf(letter) !== -1 && letter != " ") {
-                // call displayLetter
-                displayLetter(letter, blanks);
+                    // Call inputLetter
+                inputLetter(letter, userAnswerArray);
             }
-            
         }
     }
 
 
-    // Function to Display Letters - calls displayWord
-    function displayLetter(letter, blanks) {
+    // FUNCTION to input correctly answered letters into userAnswerArray
+    function inputLetter(letter, arr) {
         for (var i = 0; i < currentAnswer.length; i++) {
             if (currentAnswer[i] === letter)
-                blanks[i] = letter;
+                arr[i] = letter;
         }
-        displayWord(blanks);
+        displayWord(arr);
     }
 
-    // Function to Display Letters Guessed (not in answer)
+    // FUNCTION to Display Letters Guessed (not in answer)
     function displayLettersGuessed() {
         var divLettersGuessed = "";
         lettersGuessed.forEach(element => {
@@ -128,71 +127,104 @@ var hangman = function () {
         document.getElementById("letters-guessed").textContent = divLettersGuessed;
     }
 
-    // Function to display word/blanks after successful guesses
-    function displayWord(word) {
-        console.log(word);
+    // FUNCTION to display letters from array (for blanks or to input letter)
+    function displayWord(arr) {
+            // Dec variable to build string for display
         var newWord = '';
-        word.forEach(element => {
+            // ForEach el in array, display " ", "__", or letter (if any in array)
+        arr.forEach(element => {
             if (element === " ") {
                 newWord += "\r\n";
             }
-            else if (word.indexOf(element) !== -1 && word.indexOf(element) !== " "){
-                newWord += element + "\xa0";
-            }
-            else   {
+            else if (element === "__")  {
                 newWord += "__ ";
             }
+                // This will not run if we're displaying blanks (only when letters are guessed)
+            else if (arr.indexOf(element) !== -1 && arr.indexOf(element) !== " "){
+                newWord += element + "\xa0";
+            }
+                // For error check
+            else
+                console.log("Something went wrong");
         });
+            // make puzzle answer equal newWord
         answer.textContent = newWord;
         checkForWin();
     }
 
+    // FUNCTION to simply check if there are any blanks left (win)
     function checkForWin() {
         if (answer.textContent.indexOf("__") === -1) {
             gameOver("win");
-            
         }
     }
     
-    // GAME OVER FUNCTION
+    // FUNCTION - handles all outcomes of the game
     function gameOver(outcome) {
+            // Set variable to build img element for win/loss
         var teamImage = document.createElement('img'); 
+            // if 'win' add img src for team image using identifier from answerBank
         if (outcome === "win") {
-            // Display "identifier.svg"
+                // Build "identifier.svg"
             teamImage.src = "assets/images/teams/" + answerBank[randomNum].identifier + ".svg";
+                // Increment Win
+            wins.textContent++;
+                // JQuerty to display trophies
+            $("#trophy-right").css("visibility", "visible");
+            $("#trophy-left").css("visibility", "visible");
             
         } else {
-            // DISPLAY "lose.svg"
-            teamImage.src = "assets/images/teams/" + outcome + ".svg";
+                // Build "lose.svg"
+            teamImage.src = "assets/images/" + outcome + ".svg";
         }
-        console.log(outcome);
-        document.getElementById("win-banner").appendChild(teamImage);
-            wins.textContent++;
-            // setTimeout(() => {
-            //     resetGame()
-            // }, 5000);
+            // Display outcome banner
+        document.getElementById("outcome-banner").appendChild(teamImage);
+            
+        
+        setTimeout(() => {
+            instruction.textContent = "Press SPACE to play again..."
+            }, 2000);
+        
+            //RE INIT:  User must press SPACE to start
+        document.onkeyup = function(evt) {
+            if (evt.keyCode == 32) {
+                resetGame();
+    }
+}
         
     }
 
     // Reset Game Function (after win/loss)
     function resetGame() {
         lettersGuessed = [];
+        instruction.textContent = "Choose a Letter...";
         document.getElementById("letters-guessed").textContent = "";
         guessesRemaining.textContent = 9;
-        document.getElementById("win-banner").textContent= "";
+        document.getElementById("outcome-banner").textContent= "";
+        $("#trophy-right").css("visibility", "hidden");
+        $("#trophy-left").css("visibility", "hidden");
         hangman();
-        // pickRandomAnswer();
     }
 }
 
 //INIT:  User must press SPACE to start
 document.onkeyup = function(evt) {
     if (evt.keyCode == 32) {
-        document.getElementById("instruction").textContent = "Choose a Letter...";
-        // JQuery animation to NLF Logo (grow -> shrink)
-        $("#nfl-logo").animate({ width: "150px", height: "197"}, "normal");
-        setTimeout(() => {
-            $("#nfl-logo").animate({ width: "100px", height: "131.6px"}, "normal")}, 500);
+        instruction.textContent = "Choose a Letter...";
         hangman();
     }
 }
+
+// JQuery - when document loads
+$(document).ready ( function() {
+        // Display nfl logo after 1 sec
+    setTimeout(() => {
+        $("#nfl-logo").animate({ opacity: "1" });
+    }, 1000);
+        // JQuery animation to NLF Logo (grow -> shrink)
+    setTimeout(() => {
+        $("#nfl-logo").animate({ top: 0, width: "150px", height: "197" }, "normal")}, 1000);
+    setTimeout(() => {
+        $("#nfl-logo").animate({ top: 50, width: "100px", height: "131.6px"  }, "normal")}, 1600);
+
+});
